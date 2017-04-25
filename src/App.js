2b1424f53cb99ed6a78ea9e9ac124b6472510147
lujinming1 <lujinming163@163.com>
2017-04-25 08:42:07 -0700

    add imgFigure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import getImageURL from './util.js';
-
+import getImageURL from './util';
+import ImgFigure from './component/ImgFigure'
 //get image address
 var imageDatas = require('./data/imageDatas.json')
 //get image url
@@ -10,12 +10,19 @@ imageDatas = getImageURL(imageDatas);
 
 class App extends Component {
   render() {
+
+    var controllerUnits = [],
+        imgFigures = [];
+    imageDatas.forEach(function (value) {
+      imgFigures.push(<ImgFigure data={value} />)
+    })
     return (
       <section className="stage">
         <section className="img-sec">
-        
+          {imgFigures}
         </section>
         <nav className="controller-nav">
+        {controllerUnits}
         </nav>
       </section>
     );
